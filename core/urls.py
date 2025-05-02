cffb13cf6118e85a70e167421f8fc9f61bb666fd
Erm2k8 <ermesson2k8and@gmail.com>
2025-05-02 08:07:37 -0300

    dev: links for other pages in index

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.hello_world),
+    path('', views.hello_world, name='index'),
     path('admin/', admin.site.urls),
-    path('products/', product_views.view_all)
+    path('products/', product_views.view_all, name='products')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
